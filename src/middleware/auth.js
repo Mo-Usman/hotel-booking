576648212c0daf4dfe05cd3111e981f822988510
Muhammad Usman <muhammadusmanishaq123@gmail.com>
2024-05-22 22:26:05 +0500

    Add authentication middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/user-model')
+
+const auth = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization')
+        const decodedToken = jwt.verify(token, 'hotelapiitoken')
+        const user = await User.findOne({ _id: decodedToken._id, 'tokens.token': token })
+
+        if(!user) {
+            throw new Error()
+        }
+        
+        req.user = user
+        next()
+    } catch (e) {
+        res.send({ error: 'Please Authenticate!' })
+    }
+} 
+
+module.exports = auth
